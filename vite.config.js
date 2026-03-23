6691f8e8401579d5830c4c446366209bb4417372
val175 <val@users.noreply.github.com>
2026-03-23 17:31:25 +0800

    fix: increase pwa max file size limit to fix CI build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        maximumFileSizeToCacheInBytes: 5000000
       }
     })
   ],
